80a5126ae3c516bccedf00860411d8f2f4dd0f23
unknown <abhayrajeshshah@gmail.com>
2022-08-19 12:57:54 +0530

    fix carousel prevBtn

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-const projects = document.querySelectorAll(".project")
+const projects = document.querySelectorAll(".project");
 const nextbtn = document.getElementById("Next");
 const prevbtn = document.getElementById("Prev");
-var i=0;
+var i = 0;
 
-nextbtn.addEventListener(("click"),()=>{
-    clearInterval(interv)
-    if(i<projects.length){ 
-        i++;  
-}else{
-    i=0;
+nextbtn.addEventListener("click", () => {
+  clearInterval(interv);
+  if (i < projects.length) {
     i++;
-}
-next(i);
-interv = setInterval(helper,5000)
-})
-prevbtn.addEventListener(("click"),()=>{
-
-if(i!==0){
-    i--;
-    prev(i);
-    clearInterval(interv)
-    interv = setInterval(helper,5000)
-}
-
-})
-const helper = ()=>{
-    if(i<projects.length){ 
-        i++;  
-}else{
-    i=0;
+  } else {
+    i = 0;
     i++;
-}
-next(i);   
-}
-var interv =setInterval(helper,5000)
+  }
+  next(i);
+  interv = setInterval(helper, 5000);
+});
+prevbtn.addEventListener("click", () => {
+  if (i < 0) {
+    i = projects.length - 1;
+  }
+  i--;
+  prev(i);
+  clearInterval(interv);
+  interv = setInterval(helper, 5000);
+});
+const helper = () => {
+  if (i < projects.length) {
+    i++;
+  } else {
+    i = 0;
+    i++;
+  }
+  next(i);
+};
+var interv = setInterval(helper, 5000);
 
-const next = (i) =>{
-if(i==projects.length){
-    projects[i-1].classList.remove("active");
-    projects[0].classList.add("active") 
-}else{
-    projects[i-1].classList.remove("active");
+const next = (i) => {
+  if (i == projects.length) {
+    projects[i - 1].classList.remove("active");
+    projects[0].classList.add("active");
+  } else {
+    projects[i - 1].classList.remove("active");
     projects[i].classList.add("active");
-}
-
-}
-const prev = (i) =>{
-    if(!(i<0)){
-        projects[i+1].classList.remove("active");
-        projects[i].classList.add("active")
-    }
-}
+  }
+};
+const prev = (i) => {
+  projects[i + 1].classList.remove("active");
+  if (!(i < 0)) {
+    projects[i].classList.add("active");
+  } else {
+    projects[projects.length - 1].classList.add("active");
+  }
+};
